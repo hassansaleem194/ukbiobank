a756d163e6d824286b3ef17ab24ea76932e53069
Hassan Saleem <hassansaleem@Mac-2.lan>
2025-02-07 00:26:59 +0000

    first commit

---
+++ src/main/java/com/hassansaleem/ukbiobank/UkbiobankApplication.java
@@ -0,0 +1,13 @@
+package com.hassansaleem.ukbiobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkbiobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkbiobankApplication.class, args);
+	}
+
+}
